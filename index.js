887f33c14e1ba2b26ddf50cbe8dda3b6431fc162
Karlo Tomas <ktomas@pmfst.hr>
2022-01-12 18:29:44 +0100

    procfile i port env

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.post('/api/transakcije', (req, res) => {
   }
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
  console.log(`Posluzitelj je pokrenut na portu ${PORT}`);
 })
